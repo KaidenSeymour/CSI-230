377d99e7b23f7d1b0dcaba018aed42982c56a17e
Kaiden <kaiden.seymour@mymail.champlain.edu>
2023-10-18 14:06:13 -0400

    first log file

---
+++ week8/firstlog.ps1
@@ -0,0 +1,5 @@
+﻿Get-EventLog -List
+
+$readLog = Read-host -Prompt "Please Select a Log:"  
+
+Get-EventLog -LogName $readLog -Newest 3|Export-Csv -NoTypeInformation -Path "C:\Users\champuser\Desktop\rep.csv"
