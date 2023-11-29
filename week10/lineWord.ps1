a0bf4fe86fc6f3d393e1986bb1c48bedfad28977
Kaiden <kaiden.seymour@mymail.champlain.edu>
2023-11-29 13:20:35 -0500

    line/word counter

---
+++ week10/lineWord.ps1
@@ -0,0 +1,7 @@
+﻿$filesToCount = Get-ChildItem -Recurse -filter "*.bash"
+#Get-ChildItem
+$filesToCount
+for ($i=0; $i -lt $filesToCount.Length; $i++){
+$array = Get-Content $filesToCount[$i].FullName |  Measure-Object -Line -Word
+$array
+}
